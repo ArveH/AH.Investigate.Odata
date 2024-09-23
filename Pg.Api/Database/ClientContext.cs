ed5467803bf300314c7ac6125221ee3d40079e62
Arve Hansen <arveh4@outlook.com>
2024-09-23 12:06:25 +0200

    Added DbContext and Nugets for Postgres and EF

---
+++ Pg.Api/Database/ClientContext.cs
@@ -0,0 +1,33 @@
+﻿namespace Pg.Api.Database;
+
+public class ClientContext: DbContext
+{
+    private const string CollationName = "en_ci_as";
+
+    public ClientContext(DbContextOptions<ClientContext> options)
+        : base(options)
+    {
+        
+    }
+
+    public DbSet<Client> Clients { get; set; }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<string>().UseCollation(CollationName);
+
+        base.ConfigureConventions(configurationBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasCollation(CollationName,
+                provider: "icu",
+                locale: "en-u-ks-level1",
+                deterministic: false);
+        modelBuilder.HasCollation(CollationName,
+                provider: "icu",
+                locale: "en-u-ks-level2",
+                deterministic: false);
+    }
+}
