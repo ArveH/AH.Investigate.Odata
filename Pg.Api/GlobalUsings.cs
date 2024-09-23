eadf0876b655b751f06293879912159a02d8db09
Arve Hansen <arveh4@outlook.com>
2024-09-23 12:14:14 +0200

    Add DbContect service

--- Pg.Api/GlobalUsings.cs
+++ Pg.Api/GlobalUsings.cs
@@ -1,4 +1,5 @@
 // Global using directives
 
 global using Microsoft.EntityFrameworkCore;
+global using Pg.Api.Database;
 global using Pg.Api.Database.Entities;
--- Pg.Api/Program.cs
+++ Pg.Api/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<ClientContext>(
+    ctx => ctx.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
