554efe82327b22e52b5b56e7ac92c416112d0104
Arve Hansen <arveh4@outlook.com>
2024-09-23 11:50:09 +0200

    Added Client entity

---
+++ Pg.Api/Database/Entities/Client.cs
@@ -0,0 +1,8 @@
+﻿namespace Pg.Api.Database.Entities;
+
+public class Client
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
