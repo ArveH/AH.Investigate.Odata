e0276cabc486c6b589e55ddf0a2fa0bfc5c5738a
Arve Hansen <arveh4@outlook.com>
2024-09-23 12:25:12 +0200

    Scaffold a ClientsController

--- Pg.Api/GlobalUsings.cs
+++ Pg.Api/GlobalUsings.cs
@@ -1,5 +1,6 @@
 // Global using directives
 
+global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
 global using Pg.Api.Database;
 global using Pg.Api.Database.Entities;
