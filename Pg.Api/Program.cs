da358e865f4266cd76536b2d3f67d6e6c8e0d422
Arve Hansen <arveh@outlook.com>
2024-10-15 09:02:25 +0200

    Use CustomFilterBinder CustomQuerySqlGenerator is less code, but we override internal Npgsql classes.

--- Pg.Api/Program.cs
+++ Pg.Api/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var iLikeStrategy = LikeStrategy.CustomQuerySqlGenerator;
+var iLikeStrategy = LikeStrategy.CustomFilterBinder;
 
 builder.Services.AddControllers()
     .AddOData(opt =>
